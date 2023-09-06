202c0e556ce58e4e07d593f9b1d82187b0e40bc8
KANISHAK CHAURASIA <kanishak2020csai104@abesit.edu.in>
2023-09-06 17:08:13 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,168 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+"use client";
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+
+    const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      job: 'BlockchainEngineer',
+      resume: '',
+      phone: '',
+      github: '',
+      twitter: '',
+      website: '',
+      location: '',
+      discordUsername: '',
+      superpower: 'Code Ninja',
+    });
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      
+      // Define the API endpoint
+      const endpoint = 'https://api.verbwire.com/jobs/submit';
+  
+      try {
+        const response = await fetch(endpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+  
+        if (response.ok) {
+          console.log(response)
+          alert('Application submitted successfully!');
+          formData.Reset();
+          // You can redirect or show a success message here
+        } else {
+          alert('Failed to submit application.');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+ 
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    };
+  
+ return (
+  <div className="grid items-center min-h-screen grid-cols-1 justify-center text-center px-6 sm:px-10 md:px-20 lg:px-40 xl:px-60">
+    <h1 className="text-4xl font-bold mb-4">Verbwire Jobs</h1>
+  <form onSubmit={handleSubmit} className="flex flex-col w-full sm:w-2/3 md:w-1/2 lg:w-1/3">
+    {/* Create input fields for each form field */}
+    <label className="text-left">Name:</label>
+    <input
+      type="text"
+      name="name"
+      value={formData.name}
+      onChange={handleChange}
+      className="border border-black p-2 mb-4"
+    />
+    <label className="text-left">Email:</label>
+    <input
+      type="email"
+      name="email"
+      value={formData.email}
+      onChange={handleChange}
+      className="border border-black p-2 mb-4"
+    />
+    <label className="text-left">Job:</label>
+    <select
+      name="job"
+      value={formData.job}
+      onChange={handleChange}
+      className="border border-black p-2 mb-4"
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <option value="BlockchainEngineer">Blockchain Engineer</option>
+      <option value="FrontendDeveloper">Frontend Developer</option>
+      <option value="BackendDeveloper">Backend Developer</option>
+      <option value="FullstackDeveloper">Fullstack Developer</option>
+      <option value="DevOpsEngineer">DevOps Engineer</option>
+      <option value="CommunityManager">Community Manager</option>
+      <option value="DeveloperRelations">Developer Relations</option>
+      <option value="Other">Other</option>
+    </select>
+    <label className="text-left">Resume:</label>
+    <input
+      type="text"
+      name="resume"
+      value={formData.resume}
+      onChange={handleChange}
+      className="border border-black p-2 mb-4"
+    />
+    <label className="text-left">Phone:</label>
+    <input
+      type="text"
+      name="phone"
+      value={formData.phone}
+      onChange={handleChange}
+      className="border border-black p-2 mb-4"
+    />
+    <label className="text-left">Github:</label>
+    <input
+      type="text"
+      name="github"
+      value={formData.github}
+      onChange={handleChange}
+      className="border border-black p-2 mb-4"
+    />
+    <label className="text-left">Twitter:</label>
+    <input
+      type="text"
+      name="twitter"
+      value={formData.twitter}
+      onChange={handleChange}
+      className="border border-black p-2 mb-4"
+    />
+    <label className="text-left">Website:</label>
+    <input
+      type="text"
+      name="website"
+      value={formData.website}
+      onChange={handleChange}
+      className="border border-black p-2 mb-4"
+    />
+    <label className="text-left">Location:</label>
+    <input
+      type="text"
+      name="location"
+      value={formData.location}
+      onChange={handleChange}
+      className="border border-black p-2 mb-4"
+    />
+    <label className="text-left">Discord Username:</label>
+    <input
+      type="text"
+      name="discordUsername"
+      value={formData.discordUsername}
+      onChange={handleChange}
+      className="border border-black p-2 mb-4"
+    />
+    <label className="text-left">Superpower:</label>
+    <select
+      name="superpower"
+      value={formData.superpower}
+      onChange={handleChange}
+      className="border border-black p-2 mb-4"
+    >
+      <option value="CodeNinja">Code Ninja</option>
+      <option value="DesignWizard">Design Wizard</option>
+      <option value="CommunityHero">Community Hero</option>
+      <option value="Other">Other</option>
+    </select>
+    {/* Add a submit button */}
+    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Submit
+    </button>
+  </form>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+ )
 }
